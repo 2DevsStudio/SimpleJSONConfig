4cc4a9f1ea4e6dc9bc71c80dbf2090067a606fcf
ignitedev <ignitedevcontact@gmail.com>
2021-08-22 17:20:10 +0200

    ConfigNotFoundException

---
+++ src/main/java/com/twodevsstudio/simplejsonconfig/exceptions/ConfigNotFoundException.java
@@ -0,0 +1,11 @@
+package com.twodevsstudio.simplejsonconfig.exceptions;
+
+import java.io.File;
+
+public class ConfigNotFoundException extends RuntimeException {
+
+  public ConfigNotFoundException(File configFile) {
+    super("Config not found exception: " + configFile.getAbsolutePath());
+  }
+
+}
