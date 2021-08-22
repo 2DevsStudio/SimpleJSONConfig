155ba7b8d0dff5cdbca6e62e245db29c9b0c01e7
ignitedev <ignitedevcontact@gmail.com>
2021-08-22 20:38:57 +0200

    Loading message

--- src/main/java/com/twodevsstudio/simplejsonconfig/Main.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/Main.java
@@ -1,6 +1,12 @@
 package com.twodevsstudio.simplejsonconfig;
 
+import com.twodevsstudio.simplejsonconfig.utils.CustomLogger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    CustomLogger.log("Enabling SimpleJsonConfig - 2DevsStudio");
+  }
 }
