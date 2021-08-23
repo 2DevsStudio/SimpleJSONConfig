e4d58461aee6a8ac44743e7dbf83c06a4319f7af
slighterr12 <patrykszot.1@gmail.com>
2021-08-23 12:28:58 +0200

    Test setup and ItemStack serialization enhancements

--- src/main/java/com/twodevsstudio/simplejsonconfig/Main.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/Main.java
@@ -1,12 +1,56 @@
 package com.twodevsstudio.simplejsonconfig;
 
+import com.google.gson.GsonBuilder;
+import com.twodevsstudio.simplejsonconfig.def.DefaultGsonBuilder;
+import com.twodevsstudio.simplejsonconfig.def.Serializer;
 import com.twodevsstudio.simplejsonconfig.utils.CustomLogger;
+import com.twodevsstudio.simplejsonconfig.utils.Utils;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.util.Arrays;
 
 public class Main extends JavaPlugin {
 
   @Override
   public void onEnable() {
     CustomLogger.log("Enabling SimpleJsonConfig - 2DevsStudio");
+    getCommand("test").setExecutor(this);
+  }
+  
+  @Override
+  public boolean onCommand(@NotNull CommandSender sender,
+                           @NotNull Command command,
+                           @NotNull String label,
+                           @NotNull String[] args
+  ) {
+  
+    Player player = (Player) sender;
+    ItemStack itemStack = new ItemStack(Material.DIRT);
+    ItemMeta itemMeta = itemStack.getItemMeta();
+    itemMeta.setDisplayName(Utils.colored("&6This is Dirt"));
+    itemMeta.setLore(Utils.colored(Arrays.asList("&6This is Lore #1", "&6This is Lore #2")));
+    
+    player.getInventory().addItem(itemStack);
+    
+    File file = new File("test.json");
+    GsonBuilder gsonBuilder = new DefaultGsonBuilder().getGsonBuilder();
+    Serializer serializer = Serializer.getInst();
+    serializer.setGson(gsonBuilder.create());
+  
+    serializer.saveConfig(itemStack, file);
+  
+    ItemStack load = serializer.loadConfig(ItemStack.class, file);
+  
+    player.getInventory().addItem(load);
+  
+    return true;
   }
 }
--- src/main/java/com/twodevsstudio/simplejsonconfig/utils/Utils.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/utils/Utils.java
@@ -1,8 +1,13 @@
 package com.twodevsstudio.simplejsonconfig.utils;
 
 import lombok.experimental.UtilityClass;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class Utils {
@@ -11,5 +16,33 @@ public class Utils {
     public String colored(@NotNull String msg) {
         
         return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+    
+    @NotNull
+    public List<String> colored(@NotNull List<String> msg) {
+        
+        return msg.stream().map(Utils::colored).collect(Collectors.toList());
+    }
+    
+    @NotNull
+    public Component coloredComponent(@NotNull String msg) {
+        
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(msg);
+    }
+    
+    @NotNull
+    public List<Component> coloredComponent(@NotNull List<String> msg) {
+        
+        return msg.stream().map(Utils::coloredComponent).collect(Collectors.toList());
+    }
+    
+    public String textComponentAsString(Component component) {
+        
+        return LegacyComponentSerializer.legacyAmpersand().serialize(component);
+    }
+    
+    public List<String> textComponentAsString(List<Component> components) {
+        
+        return components.stream().map(Utils::textComponentAsString).collect(Collectors.toList());
     }
 }
