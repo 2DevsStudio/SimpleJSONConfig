11bace3dac6668dc0900e95a9148c2a52eb7512c
slighterr12 <patrykszot.1@gmail.com>
2021-08-24 13:53:00 +0200

    Ability to choose Config type on registering library

--- src/main/java/com/twodevsstudio/simplejsonconfig/SimpleJSONConfig.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/SimpleJSONConfig.java
@@ -1,40 +1,52 @@
 package com.twodevsstudio.simplejsonconfig;
 
 import com.twodevsstudio.simplejsonconfig.api.AnnotationProcessor;
+import com.twodevsstudio.simplejsonconfig.api.Config;
+import com.twodevsstudio.simplejsonconfig.def.ConfigType;
 import com.twodevsstudio.simplejsonconfig.exceptions.InstanceOverrideException;
 import com.twodevsstudio.simplejsonconfig.utils.CustomLogger;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
-import lombok.CustomLog;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public enum SimpleJSONConfig {
-  INSTANCE;
-
-  private final AnnotationProcessor annotationProcessor = new AnnotationProcessor();
-  private final Map<Plugin, File> plugins = new HashMap<>();
-
-  public void register(JavaPlugin javaPlugin, File configsDirectory) {
-
-    CustomLogger.log("Loading plugin: " + javaPlugin.getName());
-
-    if (plugins.containsKey(javaPlugin)) {
-      throw new InstanceOverrideException();
+    INSTANCE;
+    
+    private final AnnotationProcessor annotationProcessor = new AnnotationProcessor();
+    private final Map<Plugin, File> plugins = new HashMap<>();
+    
+    public void register(JavaPlugin javaPlugin, File configsDirectory) {
+        
+        CustomLogger.log("Loading plugin: " + javaPlugin.getName());
+        
+        if (plugins.containsKey(javaPlugin)) {
+            throw new InstanceOverrideException();
+        }
+        plugins.put(javaPlugin, configsDirectory);
+        
+        Set<Plugin> plugins = this.plugins.keySet();
+        
+        annotationProcessor.processAnnotations(javaPlugin, configsDirectory, plugins);
+        annotationProcessor.processAutowired(plugins);
     }
-    plugins.put(javaPlugin, configsDirectory);
-
-    Set<Plugin> plugins = this.plugins.keySet();
-
-    annotationProcessor.processAnnotations(javaPlugin, configsDirectory, plugins);
-    annotationProcessor.processAutowired(plugins);
-  }
-
-  public void register(JavaPlugin javaPlugin) {
-
-    register(javaPlugin, new File(javaPlugin.getDataFolder() + "/configuration"));
-  }
-
+    
+    public void register(JavaPlugin javaPlugin) {
+        
+        register(javaPlugin, new File(javaPlugin.getDataFolder() + "/configuration"));
+    }
+    
+    public void register(JavaPlugin javaPlugin, ConfigType configType) {
+        
+        register(javaPlugin, new File(javaPlugin.getDataFolder() + "/configuration"), configType);
+    }
+    
+    public void register(JavaPlugin javaPlugin, File configsDirectory, ConfigType configType) {
+        
+        Config.setType(configType);
+        register(javaPlugin, configsDirectory);
+    }
 }
