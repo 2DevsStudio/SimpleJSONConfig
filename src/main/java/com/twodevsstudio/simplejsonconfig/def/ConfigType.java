331194cfcb700e562fe5c1cc4e2c5a82bb741d15
slighterr12 <patrykszot.1@gmail.com>
2021-08-24 13:51:50 +0200

    Added ConfigType :: YAML / JSON

---
+++ src/main/java/com/twodevsstudio/simplejsonconfig/def/ConfigType.java
@@ -0,0 +1,13 @@
+package com.twodevsstudio.simplejsonconfig.def;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum ConfigType {
+    JSON(".json"),
+    YAML(".yml");
+    
+    private final String extension;
+}
