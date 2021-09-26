0db2f8ffcf7f3d225ad779e0cc1f89f7a50dac2d
ignitedev <ignitedevcontact@gmail.com>
2021-09-27 00:45:51 +0200

    Component support for ItemstackAdapter

--- src/main/java/com/twodevsstudio/simplejsonconfig/def/adapters/ItemStackAdapter.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/def/adapters/ItemStackAdapter.java
@@ -7,6 +7,18 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
+import com.google.gson.internal.LinkedTreeMap;
+import com.twodevsstudio.simplejsonconfig.utils.MetaSerializationUtils;
+import com.twodevsstudio.simplejsonconfig.utils.TextUtility;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.attribute.AttributeModifier.Operation;
@@ -15,157 +27,229 @@ import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
 import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
 
 public class ItemStackAdapter implements JsonSerializer<ItemStack>, JsonDeserializer<ItemStack> {
 
-    private static final String CLASS_KEY = "SERIAL-ADAPTER-CLASS-KEY";
+  private static final String CLASS_KEY = "SERIAL-ADAPTER-CLASS-KEY";
 
-    private final Type seriType = new TypeToken<Map<String, Object>>() {
-    }.getType();
+  private final Type seriType = new TypeToken<Map<String, Object>>() {
+  }.getType();
 
-    @Override
-    public ItemStack deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+  private final String META_TYPE_MEMBER = "meta-type";
+  private final String AMOUNT_MEMBER = "amount";
+  private final String META_MEMBER = "meta";
+  private final String ATTRIBUTES_MEMBER = "attribute-modifiers";
+  private final String DISPLAY_NAME_MEMBER = "display-name";
+  private final String LORE_MEMBER = "lore";
+  private final String CUSTOM_EFFECTS_MEMBER = "custom-effects";
 
-        Map<String, Object> itemData = context.deserialize(json, seriType);
+  @Override
+  public JsonElement serialize(ItemStack item, Type typeOfSrc, JsonSerializationContext context) {
 
-        if (itemData.get("amount") != null) {
-            Integer amount = ((Number) itemData.get("amount")).intValue();
-            itemData.put("amount", amount);
-        }
+    Map<String, Object> jsonSerialized = getJsonSerialized(item);
+    return context.serialize(jsonSerialized);
+  }
 
-        ItemStack item = ItemStack.deserialize(itemData);
+  @Override
+  public ItemStack deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+      throws JsonParseException {
 
-        if (itemData.containsKey("meta")) {
-            Map<String, Object> itemmeta = (Map<String, Object>) itemData.get("meta");
-            itemmeta = recursiveDoubleToInteger(itemmeta);
+    Map<String, Object> itemData = context.deserialize(json, seriType);
 
-
-            ItemMeta meta = (ItemMeta) ConfigurationSerialization.deserializeObject(
-                    itemmeta, Objects.requireNonNull(ConfigurationSerialization.getClassByAlias("ItemMeta")));
-
-            Map<String, Object> attributes = (Map<String, Object>) itemmeta.get("attribute-modifiers");
-            deserializeAttributes(attributes, meta);
-            item.setItemMeta(meta);
-        }
-
-        return item;
+    if (itemData.get(AMOUNT_MEMBER) != null) {
+      Integer amount = ((Number) itemData.get(AMOUNT_MEMBER)).intValue();
+      itemData.put(AMOUNT_MEMBER, amount);
     }
 
+    ItemStack item = ItemStack.deserialize(itemData);
 
-    @Override
-    public JsonElement serialize(ItemStack item, Type typeOfSrc, JsonSerializationContext context) {
-
-        Map<String, Object> jsonSerialized = getJsonSerialized(item);
-        return context.serialize(jsonSerialized);
+    if (itemData.containsKey(META_MEMBER)) {
+      deserializeMeta(itemData, item);
     }
 
-    private Map<String, Object> getJsonSerialized(@NotNull ItemStack item) {
+    return item;
+  }
 
-        Map<String, Object> serial = item.serialize();
+  private Map<String, Object> getJsonSerialized(@NotNull ItemStack item) {
 
-        if (serial.get("meta") != null) {
-            serial.put("meta", serializeMeta(Objects.requireNonNull(item.getItemMeta())));
-        }
-        return serial;
+    Map<String, Object> serial = item.serialize();
+
+    if (serial.get(META_MEMBER) != null) {
+      serial.put(META_MEMBER, serializeMeta(Objects.requireNonNull(item.getItemMeta())));
+    }
+    return serial;
+  }
+
+  private Map<String, Object> serializeMeta(@NotNull ItemMeta itemMeta) {
+
+    Map<String, Object> originalMeta = itemMeta.serialize();
+    Map<String, Object> meta = new HashMap<>(originalMeta);
+
+    for (Map.Entry<String, Object> entry : meta.entrySet()) {
+
+      String key = entry.getKey();
+      Object object = entry.getValue();
+
+      //Plain strings with ampersand color codes instead of Text Components
+      if (key.equalsIgnoreCase(DISPLAY_NAME_MEMBER)) {
+        meta.put(key, TextUtility.textComponentAsString(itemMeta.displayName()));
+        continue;
+      }
+
+      if (key.equalsIgnoreCase(LORE_MEMBER)) {
+        meta.put(key, TextUtility.textComponentAsString(itemMeta.lore()));
+        continue;
+      }
+
+      if (object instanceof ConfigurationSerializable) {
+        ConfigurationSerializable serializable = (ConfigurationSerializable) object;
+        Map<String, Object> serialized = recursiveSerialization(serializable);
+        meta.put(key, serialized);
+      }
+    }
+    return meta;
+  }
+
+  @NotNull
+  private Map<String, Object> recursiveSerialization(
+      @NotNull ConfigurationSerializable configurationSerializable) {
+
+    Map<String, Object> originalMap = configurationSerializable.serialize();
+    Map<String, Object> map = new HashMap<>();
+
+    for (Map.Entry<String, Object> entry : originalMap.entrySet()) {
+      Object object = entry.getValue();
+      if (object instanceof ConfigurationSerializable) {
+        ConfigurationSerializable serializable = (ConfigurationSerializable) object;
+        Map<String, Object> newMap = recursiveSerialization(serializable);
+        newMap.put(CLASS_KEY, ConfigurationSerialization.getAlias(serializable.getClass()));
+        map.put(entry.getKey(), newMap);
+      }
     }
 
-    private void deserializeAttributes(Map<String, Object> attributes, ItemMeta meta) {
+    map.put(CLASS_KEY,
+        ConfigurationSerialization.getAlias(configurationSerializable.getClass()));
+    return map;
+  }
 
-        for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+  private void deserializeMeta(Map<String, Object> itemData, ItemStack item) {
 
-            String attributeName = entry.getKey();
+    Map<String, Object> rawMeta = (Map<String, Object>) itemData.get(META_MEMBER);
+    String metaType = (String) rawMeta.get(META_TYPE_MEMBER);
 
-            Attribute attribute = Attribute.valueOf(attributeName);
-            List<Map<String, Object>> rawModifiers = (List<Map<String, Object>>) entry.getValue();
-            List<AttributeModifier> modifiers = new ArrayList<>();
+    rawMeta = recursiveDoubleToInteger(rawMeta);
 
-            for (Map<String, Object> rawModifier : rawModifiers) {
+    String displayName = (String) rawMeta.get(DISPLAY_NAME_MEMBER);
+    List<String> lore = (List<String>) rawMeta.get(LORE_MEMBER);
 
-                UUID modifierUUID = UUID.fromString(String.valueOf(rawModifier.get("uuid")));
-                String modifierName = String.valueOf(rawModifier.get("name"));
-                double modifierAmount = (double) rawModifier.get("amount");
-                Operation modifierOperation = Operation.valueOf(String.valueOf(rawModifier.get("operation")));
+    //removing name and lore members to avoid incorrect meta deserialization
+    rawMeta.remove(DISPLAY_NAME_MEMBER);
+    rawMeta.remove(LORE_MEMBER);
 
+    deserializeMetaTypeData(rawMeta, metaType);
 
-                EquipmentSlot modifierSlot = null;
-                Object rawSlot = rawModifier.get("slot");
-                if (rawSlot != null) {
-                    modifierSlot = EquipmentSlot.valueOf(String.valueOf(rawSlot));
-                }
+    ItemMeta meta = (ItemMeta) ConfigurationSerialization.deserializeObject(
+        rawMeta,
+        Objects.requireNonNull(ConfigurationSerialization.getClassByAlias("ItemMeta")));
 
-                modifiers.add(new AttributeModifier(modifierUUID, modifierName, modifierAmount, modifierOperation, modifierSlot));
-            }
-            modifiers.forEach(modifier -> meta.addAttributeModifier(attribute, modifier));
-        }
+    if (meta != null) {
+      if (displayName != null) {
+        meta.displayName(TextUtility.coloredComponent(displayName));
+      }
+      if (lore != null) {
+        meta.lore(TextUtility.coloredComponent(lore));
+      }
     }
 
-    private Map<String, Object> serializeMeta(@NotNull ItemMeta itemMeta) {
+    Map<String, Object> attributes = (Map<String, Object>) rawMeta.getOrDefault(
+        ATTRIBUTES_MEMBER, new HashMap<>());
+    deserializeAttributes(attributes, meta);
+    item.setItemMeta(meta);
+  }
 
-        Map<String, Object> originalMeta = itemMeta.serialize();
-        Map<String, Object> meta = new HashMap<>(originalMeta);
+  private void deserializeMetaTypeData(Map<String, Object> rawMeta, String metaType) {
 
-        for (Map.Entry<String, Object> entry : meta.entrySet()) {
+    if (metaType.equalsIgnoreCase("POTION")) {
 
-            String key = entry.getKey();
-            Object object = entry.getValue();
-            if (object instanceof ConfigurationSerializable) {
-                ConfigurationSerializable serializable = (ConfigurationSerializable) object;
-                Map<String, Object> serialized = recursiveSerialization(serializable);
-                meta.put(key, serialized);
-            }
-        }
-        return meta;
+      List<PotionEffect> potionEffects = MetaSerializationUtils.deserializePotionEffects(
+          (List<LinkedTreeMap<String, Object>>) rawMeta.get(CUSTOM_EFFECTS_MEMBER));
+
+      rawMeta.put(CUSTOM_EFFECTS_MEMBER, potionEffects);
+    }
+  }
+
+  /**
+   * ItemStack deserialization does not support double values in most cases This method recursively
+   * converts all double values to integer in original and nested maps
+   *
+   * @return Original map with converted doubles to integers
+   */
+  @NotNull
+  private Map<String, Object> recursiveDoubleToInteger(@NotNull Map<String, Object> originalMap) {
+
+    Map<String, Object> map = new HashMap<>();
+    for (Map.Entry<String, Object> entry : originalMap.entrySet()) {
+
+      Object object = entry.getValue();
+
+      if (object instanceof Double) {
+        int number = ((Double) object).intValue();
+        map.put(entry.getKey(), number);
+
+      } else if (object instanceof Map) {
+        Map<String, Object> subMap = (Map<String, Object>) object;
+        map.put(entry.getKey(), recursiveDoubleToInteger(subMap));
+      } else {
+        map.put(entry.getKey(), object);
+      }
+    }
+    return map;
+  }
+
+  private void deserializeAttributes(Map<String, Object> attributes, ItemMeta meta) {
+
+    Set<UUID> uuidsInUse = new HashSet<>();
+    for (Map.Entry<String, Object> entry : attributes.entrySet()) {
+
+      String attributeName = entry.getKey();
+      Attribute attribute = Attribute.valueOf(attributeName);
+
+      List<Map<String, Object>> rawModifiers = (List<Map<String, Object>>) entry.getValue();
+      List<AttributeModifier> modifiers = new ArrayList<>();
+
+      for (Map<String, Object> rawModifier : rawModifiers) {
+        deserializeModifier(rawModifier, modifiers, uuidsInUse);
+      }
+      modifiers.forEach(modifier -> meta.addAttributeModifier(attribute, modifier));
+    }
+  }
+
+  private void deserializeModifier(Map<String, Object> rawModifier,
+      List<AttributeModifier> modifiers,
+      Set<UUID> uuidsInUse
+  ) {
+
+    UUID modifierUUID = UUID.fromString((String) rawModifier.get("uuid"));
+    if (uuidsInUse.contains(modifierUUID)) {
+      modifierUUID = UUID.randomUUID();
+    }
+    uuidsInUse.add(modifierUUID);
+
+    String modifierName = String.valueOf(rawModifier.get("name"));
+    double modifierAmount = (double) rawModifier.get(AMOUNT_MEMBER);
+    Operation modifierOperation = Operation.valueOf(
+        String.valueOf(rawModifier.get("operation")));
+
+    EquipmentSlot modifierSlot = null;
+    Object rawSlot = rawModifier.get("slot");
+    if (rawSlot != null) {
+      modifierSlot = EquipmentSlot.valueOf(String.valueOf(rawSlot));
     }
 
-    @NotNull
-    private Map<String, Object> recursiveSerialization(@NotNull ConfigurationSerializable configurationSerializable) {
-
-        Map<String, Object> originalMap = configurationSerializable.serialize();
-        Map<String, Object> map = new HashMap<>();
-
-        for (Map.Entry<String, Object> entry : originalMap.entrySet()) {
-            Object object = entry.getValue();
-            if (object instanceof ConfigurationSerializable) {
-                ConfigurationSerializable serializable = (ConfigurationSerializable) object;
-                Map<String, Object> newMap = recursiveSerialization(serializable);
-                newMap.put(CLASS_KEY, ConfigurationSerialization.getAlias(serializable.getClass()));
-                map.put(entry.getKey(), newMap);
-            }
-        }
-
-        map.put(CLASS_KEY, ConfigurationSerialization.getAlias(configurationSerializable.getClass()));
-        return map;
-    }
-
-    @NotNull
-    private Map<String, Object> recursiveDoubleToInteger(@NotNull Map<String, Object> originalMap) {
-
-        Map<String, Object> map = new HashMap<>();
-        for (Map.Entry<String, Object> entry : originalMap.entrySet()) {
-
-            Object object = entry.getValue();
-
-            if (object instanceof Double) {
-                int number = ((Double) object).intValue();
-                map.put(entry.getKey(), number);
-
-            } else if (object instanceof Map) {
-                Map<String, Object> subMap = (Map<String, Object>) object;
-                map.put(entry.getKey(), recursiveDoubleToInteger(subMap));
-            } else {
-                map.put(entry.getKey(), object);
-            }
-        }
-        return map;
-    }
-
+    modifiers.add(
+        new AttributeModifier(modifierUUID, modifierName, modifierAmount, modifierOperation,
+            modifierSlot));
+  }
 }
