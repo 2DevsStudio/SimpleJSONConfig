ad3fda18bec4acab3101730f7690aa6454196a73
ignitedev <ignitedevcontact@gmail.com>
2021-09-27 00:43:16 +0200

    Component support for TextUtility

--- src/main/java/com/twodevsstudio/simplejsonconfig/utils/TextUtility.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/utils/TextUtility.java
@@ -3,6 +3,8 @@ package com.twodevsstudio.simplejsonconfig.utils;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.jetbrains.annotations.NotNull;
 
@@ -25,5 +27,25 @@ public class TextUtility {
   @NotNull
   public List<String> toAmpersand(@NotNull List<String> msg) {
     return msg.stream().map(TextUtility::toAmpersand).collect(Collectors.toList());
+  }
+
+  @NotNull
+  public Component coloredComponent(@NotNull String msg) {
+    return LegacyComponentSerializer.legacyAmpersand().deserialize(msg);
+  }
+
+  @NotNull
+  public List<Component> coloredComponent(@NotNull List<String> msg) {
+    return msg.stream().map(TextUtility::coloredComponent).collect(Collectors.toList());
+  }
+
+  public String textComponentAsString(Component component) {
+
+    return LegacyComponentSerializer.legacyAmpersand().serialize(component);
+  }
+
+  public List<String> textComponentAsString(List<Component> components) {
+
+    return components.stream().map(TextUtility::textComponentAsString).collect(Collectors.toList());
   }
 }
