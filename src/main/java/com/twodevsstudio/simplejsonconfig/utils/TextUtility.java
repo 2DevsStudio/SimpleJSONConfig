60cc9882d6037b09938fa2a2ff18928773d8bb1c
ignitedev <ignitedevcontact@gmail.com>
2021-09-27 00:40:05 +0200

    TextUtility to ampersand

--- src/main/java/com/twodevsstudio/simplejsonconfig/utils/TextUtility.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/utils/TextUtility.java
@@ -4,15 +4,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 import org.bukkit.ChatColor;
+import org.jetbrains.annotations.NotNull;
 
 @UtilityClass
 public class TextUtility {
 
-    public String colorize(String toColor) {
-        return ChatColor.translateAlternateColorCodes('&', toColor);
-    }
+  public String colorize(String toColor) {
+    return ChatColor.translateAlternateColorCodes('&', toColor);
+  }
 
-    public List<String> colorize(List<String> toColor) {
-        return toColor.stream().map(TextUtility::colorize).collect(Collectors.toList());
-    }
+  public List<String> colorize(List<String> toColor) {
+    return toColor.stream().map(TextUtility::colorize).collect(Collectors.toList());
+  }
+
+  @NotNull
+  public String toAmpersand(@NotNull String msg) {
+    return msg.replace("§", "&");
+  }
+
+  @NotNull
+  public List<String> toAmpersand(@NotNull List<String> msg) {
+    return msg.stream().map(TextUtility::toAmpersand).collect(Collectors.toList());
+  }
 }
