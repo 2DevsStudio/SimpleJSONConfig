0bb70b0ba86d8aaaba5e80f71065e26133d20334
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-07-22 13:30:08 +0200

    You can now specify configPath in @Configuration

--- src/main/java/com/twodevsstudio/simplejsonconfig/interfaces/Configuration.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/interfaces/Configuration.java
@@ -3,28 +3,37 @@ package com.twodevsstudio.simplejsonconfig.interfaces;
 import java.lang.annotation.*;
 
 /**
- * @apiNote Annotate any class to make it a {@code Configuration} Configuration classes have to extend a {@code Config}
- * abstract class to inherit its methods and make it able to be processes by AnnotationProcessor
+ * @apiNote Annotate any class to make it a {@code Configuration} Configuration classes have to
+ *     extend a {@code Config} abstract class to inherit its methods and make it able to be
+ *     processes by AnnotationProcessor
  */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface Configuration {
 
-    /**
-     * You have to specify your configuration name.
-     *
-     * @return name of the configuration
-     * @apiNote It's also the name of your configuration file
-     */
-    String value();
+  /**
+   * You have to specify your configuration name.
+   *
+   * @return name of the configuration
+   * @apiNote It's also the name of your configuration file
+   */
+  String value();
 
-    /**
-     * If true: Enables automatic config updates for existing configuration files After changing the definition of the
-     * config class, changes will be automatically applied to existing configuration files. ie. After adding new field
-     * to the configuration it will be automatically written to your config file with default value (or null if default
-     * was not specified)
-     */
-    boolean enableConfigAutoUpdates() default true;
+  /**
+   * You can specify a path to your configuration file
+   *
+   * @implNote Child path, starting from the plugins folder, example: "/simplejsonconfig", if not
+   *     specified it will be the default to directory set in SimpleJSONConfig initializing
+   *     (SimpleJSONConfig.INSTANCE.register)
+   */
+  String configPath() default "";
 
+  /**
+   * If true: Enables automatic config updates for existing configuration files After changing the
+   * definition of the config class, changes will be automatically applied to existing configuration
+   * files. ie. After adding new field to the configuration it will be automatically written to your
+   * config file with default value (or null if default was not specified)
+   */
+  boolean enableConfigAutoUpdates() default true;
 }
