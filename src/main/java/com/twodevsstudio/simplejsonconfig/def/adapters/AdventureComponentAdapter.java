9945160cf6d748f953a5522e0fb199c76d70a890
IM-IgniteDEV <IgniteDEVContact@gmail.com>
2025-08-22 12:16:22 +0200

    Refactor AdventureComponentAdapter to use MiniMessage for serialization and deserialization

--- src/main/java/com/twodevsstudio/simplejsonconfig/def/adapters/AdventureComponentAdapter.java
+++ src/main/java/com/twodevsstudio/simplejsonconfig/def/adapters/AdventureComponentAdapter.java
@@ -2,21 +2,29 @@ package com.twodevsstudio.simplejsonconfig.def.adapters;
 
 import com.google.gson.*;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 
 import java.lang.reflect.Type;
 
-public class AdventureComponentAdapter implements JsonSerializer<Component>, JsonDeserializer<Component> {
+public class AdventureComponentAdapter
+    implements JsonSerializer<Component>, JsonDeserializer<Component> {
 
-  private static final GsonComponentSerializer SERIALIZER = GsonComponentSerializer.gson();
+  private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
   @Override
   public JsonElement serialize(Component src, Type typeOfSrc, JsonSerializationContext context) {
-    return JsonParser.parseString(SERIALIZER.serialize(src));
+    return new JsonPrimitive(MINI_MESSAGE.serialize(src));
   }
 
   @Override
-  public Component deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-    return SERIALIZER.deserialize(json.toString());
+  public Component deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context)
+      throws JsonParseException {
+    if (json == null || json.isJsonNull()) {
+      return Component.empty();
+    }
+    if (!json.isJsonPrimitive() || !json.getAsJsonPrimitive().isString()) {
+      throw new JsonParseException("Expected MiniMessage string for Component");
+    }
+    return MINI_MESSAGE.deserialize(json.getAsString());
   }
 }
